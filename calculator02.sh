577796c8ccd2d7e194444a1365a6ec23ea536f15
xiaoming <123456@163.com>
2020-11-12 14:22:52 +0800

    calculator02.sh

---
+++ calculator02.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Date: 2020-11-12
+# Author: zoulongbin
+# Description: + - * calculator
+# Version: 2.0
+
+print_usage() {
+  echo -e "Please enter integer\n"
+  exit 1
+}
+
+read -p "Please input first number: " firstnum
+read -p "Please input second number: " secondnum
+
+expr $firstnum + 1 > /dev/null 2>&1 && [ $? -eq 0 ] || print_usage
+expr $secondnum + 1 > /dev/null 2>&1 && [ $? -eq 0 ] || print_usage
+
+echo "$firstnum + $secondnum = $(($firstnum+$secondnum))"
