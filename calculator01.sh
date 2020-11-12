4243f9682f53ecb5f81e1234da0bdb010435a555
xiaoming <123456@163.com>
2020-11-12 14:28:19 +0800

    modify calculator name

--- calculator01.sh
+++ calculator01.sh
@@ -1,29 +1,19 @@
 #!/bin/bash
 
-# Date: 2020-11-22
+# Date: 2020-11-12
 # Author: zoulongbin
 # Description: + - * calculator
-# Version: 2.0
+# Version: 1.0
 
 print_usage() {
-  echo -e "Please enter an integer\n"
+  echo -e "Please enter integer\n"
   exit 1
 }
 
-read -p "Pleaser input first number: " firstnum
-if [ -n "`echo ${firstnum} | sed 's/[0-9]//g'`" ];then
-  print_usage
-fi
+read -p "Please input first number: " firstnum
+read -p "Please input second number: " secondnum
 
-read -p "Pleaser input operators: " operator
-if [ "$operator" != "+" ] && [ "$operator" != "-" ] && [ "$operator" != "*" ];then
-  echo "Please user {+|-|*}"
-  exit 2
-fi
+expr $firstnum + 1 > /dev/null 2>&1 && [ $? -eq 0 ] || print_usage
+expr $secondnum + 1 > /dev/null 2>&1 && [ $? -eq 0 ] || print_usage
 
-read -p "Pleaser input second number: " secondnum
-if [ -n "`echo ${secondnum} | sed 's/[0-9]//g'`" ];then
-  print_usage
-fi
-
-echo "${firstnum} ${operator} ${secondnum} = $((${firstnum}${operator}${secondnum}))"
+echo "$firstnum + $secondnum = $(($firstnum+$secondnum))"
