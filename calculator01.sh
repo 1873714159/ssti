c88cb851160c04421974fba4b4a38e92e172d2e9
xiaoming <123456@163.com>
2020-11-12 15:39:34 +0800

    modify

--- calculator01.sh
+++ calculator01.sh
@@ -11,9 +11,22 @@ print_usage() {
 }
 
 read -p "Please input first number: " firstnum
+
+[ ${#firstnum} -le 0 ] && {
+  echo "The first number is not null"
+  exit 1
+}
+
 read -p "Please input second number: " secondnum
 
+[ ${#secondnum} -le 0 ] && {
+  echo "Then second number is not null"
+  exit 1
+}
+
 expr $firstnum + 1 > /dev/null 2>&1 && [ $? -eq 0 ] || print_usage
+
 expr $secondnum + 1 > /dev/null 2>&1 && [ $? -eq 0 ] || print_usage
+
 
 echo "$firstnum + $secondnum = $(($firstnum+$secondnum))"
