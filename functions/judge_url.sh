e5cf11383140038ce7b9af8876bf1435938ac1ba
xiaoming <123456@qq.com>
2020-11-16 16:02:24 +0800

    modify

---
+++ functions/judge_url.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Date: 2020-11-16
+# Author: zoulongbin
+# Description: judge url address
+# Version: 1.0
+
+. /etc/init.d/functions
+
+
+function usage() {
+  echo $"Usage: $0 url"
+  exit 1
+}
+
+function check_url() {
+  curl -s -o /dev/null $1
+  if [ $? -eq 0 ]
+    then
+      action "$1 is yes" /bin/true
+  else
+      action "$1 is no" /bin/false
+  fi
+}
+
+function main() {
+   if [ $# -eq 0 ]
+     then
+        usage
+   fi
+
+   check_url $1
+}
+
+main $*
