83337291bcb59c16d87e64ef4cf8348e9b908b0b
xiaoming <123456@qq.com>
2020-11-24 17:06:22 +0800

    modify

--- instance/system_check.sh
+++ instance/system_check.sh
@@ -45,63 +45,33 @@ bootdir_prompt=`df -h | grep -w '/boot' | awk '{print $5}' | tr -d "%"`
 function MemoryCheck() {
 
 ### Memory available less than 256M
-if [ $memava_prompt -lt 256000 ]
-  then
-    echo -e "${FLASH} ${RED} *** Available Memory less than 256M *** ${RES}"
-fi
+[ $memava_prompt -lt 256000 ]  && echo -e "${FLASH} ${RED} *** Available Memory less than 256M *** ${RES}"
  
 }
+
 
 function CPUCheck() {
 
 ### two core CPU
 if [ $cpupercore -eq 2 ];then
- if [ `expr ${cpuload_prompt[0]} \> 3.0` -eq 1 ]
-    then
-      echo -e "${FLASH} ${RED} CPU one minute load greater than 3.0 ${RES}"
- elif [ `expr ${cpuload_prompt[1]} \> 2.0` -eq 1 ]
-    then   
-      echo -e "${FLASH} ${RED} CPU five minutes load greater than 2.0 ${RES}"
- elif [ `expr ${cpuload_prompt[2]} \> 2.0` -eq 1 ] 
-    then
-      echo -e "${FLASH} ${RED} CPU fifteen minutes load greater than 2.0 ${RES}"
- fi
+[ `expr ${cpuload_prompt[0]} \> 3.0` -eq 1 ] && echo -e "${FLASH} ${RED} *** CPU one minute load greater than 3.0 *** ${RES}"
+[ `expr ${cpuload_prompt[1]} \> 2.0` -eq 1 ] &&  echo -e "${FLASH} ${RED} CPU five minutes load greater than 2.0 ${RES}"
+[ `expr ${cpuload_prompt[2]} \> 2.0` -eq 1 ] && echo -e "${FLASH} ${RED} CPU fifteen minutes load greater than 2.0 ${RES}"
 ### four core CPU
 elif [ $cpupercore -eq 4 ];then
- if [ `expr ${cpuload_prompt[0]} \> 6.0` -eq 1 ]
-    then
-      echo -e "${FLASH} ${RED} CPU one minute load greater than 6.0 ${RES}"
- elif [ `expr ${cpuload_prompt[1]} \> 4.0` -eq 1 ]
-    then   
-      echo -e "${FLASH} ${RED} CPU five minutes load greater than 4.0 ${RES}"
- elif [ `expr ${cpuload_prompt[2]} \> 4.0` -eq 1 ] 
-    then
-      echo -e "${FLASH} ${RED} CPU fifteen minutes load greater than 4.0 ${RES}"
- fi
+[ `expr ${cpuload_prompt[0]} \> 6.0` -eq 1 ] && echo -e "${FLASH} ${RED} *** CPU one minute load greater than 6.0 *** ${RES}"
+[ `expr ${cpuload_prompt[1]} \> 4.0` -eq 1 ] &&  echo -e "${FLASH} ${RED} CPU five minutes load greater than 4.0 ${RES}"
+[ `expr ${cpuload_prompt[2]} \> 4.0` -eq 1 ] && echo -e "${FLASH} ${RED} CPU fifteen minutes load greater than 4.0 ${RES}"
 ### eight core CPU
 elif [ $cpupercore -eq 8 ];then
- if [ `expr ${cpuload_prompt[0]} \> 10.0` -eq 1 ]
-    then
-      echo -e "${FLASH} ${RED} CPU one minute load greater than 10.0 ${RES}"
- elif [ `expr ${cpuload_prompt[1]} \> 8.0` -eq 1 ]
-    then   
-      echo -e "${FLASH} ${RED} CPU five minutes load greater than 8.0 ${RES}"
- elif [ `expr ${cpuload_prompt[2]} \> 8.0` -eq 1 ] 
-    then
-      echo -e "${FLASH} ${RED} CPU fifteen minutes load greater than 8.0 ${RES}"
- fi
+[ `expr ${cpuload_prompt[0]} \> 10.0` -eq 1 ] && echo -e "${FLASH} ${RED} *** CPU one minute load greater than 10.0 *** ${RES}"
+[ `expr ${cpuload_prompt[1]} \> 8.0` -eq 1 ] &&  echo -e "${FLASH} ${RED} CPU five minutes load greater than 8.0 ${RES}"
+[ `expr ${cpuload_prompt[2]} \> 8.0` -eq 1 ] && echo -e "${FLASH} ${RED} CPU fifteen minutes load greater than 8.0 ${RES}"
 ### sixteen core CPU
 elif [ $cpupercore -eq 16 ];then
- if [ `expr ${cpuload_prompt[0]} \> 20.0` -eq 1 ]
-    then
-      echo -e "${FLASH} ${RED} CPU one minute load greater than 20.0 ${RES}"
- elif [ `expr ${cpuload_prompt[1]} \> 16.0` -eq 1 ]
-    then   
-      echo -e "${FLASH} ${RED} CPU five minutes load greater than 16.0 ${RES}"
- elif [ `expr ${cpuload_prompt[2]} \> 16.0` -eq 1 ] 
-    then
-      echo -e "${FLASH} ${RED} CPU fifteen minutes load greater than 16.0 ${RES}"
- fi
+[ `expr ${cpuload_prompt[0]} \> 20.0` -eq 1 ] && echo -e "${FLASH} ${RED} *** CPU one minute load greater than 20.0 *** ${RES}"
+[ `expr ${cpuload_prompt[1]} \> 16.0` -eq 1 ] &&  echo -e "${FLASH} ${RED} CPU five minutes load greater than 16.0 ${RES}"
+[ `expr ${cpuload_prompt[2]} \> 16.0` -eq 1 ] && echo -e "${FLASH} ${RED} CPU fifteen minutes load greater than 16.0 ${RES}"
 fi
 
 }
@@ -110,14 +80,8 @@ fi
 function  DiskCheck() {
 
 ### root dir and boot dir percent greater than 90%
-if [ $rootdir_prompt -gt 90 ]
-  then 
-    echo -e "${FLASH} ${RED} "/" root directory percent greater than 90% ${RES}"
-elif [ $bootdir_prompt -gt 60 ]
-  then
-    echo -e "${FLASH} ${RED} "/" boot directory percent greater than 60% ${RES}"
-fi
-
+[ $rootdir_prompt -gt 90 ] && echo -e "${FLASH} ${RED} "/" root directory percent greater than 90% ${RES}"
+[ $bootdir_prompt -gt 60 ] && echo -e "${FLASH} ${RED} "/" boot directory percent greater than 60% ${RES}"
  
 }
 
@@ -132,6 +96,7 @@ echo "### sys runtime:   ${sysruntime} days"
 echo "### scripts path:  /etc/profile.d/SystemCheck.sh"
 echo "##############################################"
 echo
+echo
 
 echo "CPU型号:                " $cputype 
 echo "CPU数量:                " $cpunumber
@@ -139,11 +104,13 @@ echo "CPU每个核数：           " $cpupercore
 echo "CPU负载:                " $cpuload
 CPUCheck
 echo
+echo
 
 echo "内存容量:               "  $memtoal
 echo "内存已用:               "  $memused
 echo "内存可用:               "  $memavailable
 MemoryCheck
+echo
 echo
 
 echo "磁盘/根目录容量:        "  $disktotal
@@ -151,4 +118,5 @@ echo "磁盘/根目录已用:        "  $diskuserd
 echo "磁盘/根目录可用:        "  $diskfree
 echo "磁盘/根目录使用百分比:  "  $diskper
 DiskCheck
+echo
 echo
